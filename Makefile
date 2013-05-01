916ce6cf2183795ab5703cd3140158bdf0dbd587
マ・ベンソン <bm@Lord-Alec-Magos-Pasions-iMac.local>
2013-05-01 18:55:52 +0900

    added LAPACK for eigenvector decomposition and added covariance

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CC = c++
 CFLAGS = -c -I/opt/local/include/openbabel-2.0/
-LDFLAGS = -L/opt/local/lib/ -lopenbabel -lcblas
+LDFLAGS = -L/opt/local/lib/ -lopenbabel -lcblas -lclapack
+#-framework Accelerate
 
 all: example
 
--- example.cpp
+++ example.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <limits>
 #include <cmath>
-#include <Accelerate/Accelerate.h>
 #include <openbabel/obconversion.h>
 #include <openbabel/mol.h>
 #include <openbabel/data.h>
+
+#ifdef __APPLE__
+    #include <Accelerate/Accelerate.h>
+#else
+    #include <cblas.h>
+    #include <clapack.h>
+#endif
 
 using namespace std;
 using namespace OpenBabel;
@@ -39,13 +46,32 @@ double volumeOverlap (OBMol &moleculeA, OBMol &moleculeB) {
 }
 
 
-void generateCoordsMatrixFromMolecule(vector<double> &matrix, OBMol &molecule) {
-    // generates column-order matrix of coordinates
-    matrix.clear();
+
+void generateCoordsMatrixFromMolecule(vector<double> &matrix, OBMol &molecule) {     // generates column-order matrix of coordinates
+    matrix.clear(); matrix.insert(matrix.end(), molecule.GetCoordinates(), &molecule.GetCoordinates()[3*molecule.NumAtoms()]);
+}
+
+void saveCoordsMatrixToMolecule(OBMol &molecule, vector<double> &matrix) {
+    //if (matrix.size()/3 != )
+}
+
+
+void generateEigenMatrix(vector<double> &eigenvectors, vector<double> &eigenvalues, const vector<double> &matrix) {
+    // http://www.netlib.org/lapack/explore-html/d2/d8a/group__double_s_yeigen.html#gaeed8a131adf56eaa2a9e5b1e0cce5718  |  http://www.ualberta.ca/~kbeach/lapack.html
+    int matrixOrder = 3;
+    eigenvalues.clear(); eigenvalues.resize(3, 0);
+    eigenvectors.clear(); eigenvectors = matrix;
+    int lwork = 102, info; vector<double> work(lwork); // LAPACK parameters; work[0] will show the optimum value for lwork when call completes
+    if (dsyev_("V", "L", &matrixOrder, &eigenvectors[0], &matrixOrder, &eigenvalues[0], &work[0], &lwork, &info)) { cerr << "ERROR: NO MATRIX INITIALIZED IN POINTER; EXITING" << endl; abort(); }
+}
+
+void getMoleculeCenterCoords(vector<double> &centerCoords, OBMol &molecule) {
+    centerCoords.clear(); centerCoords.resize(3, 0);
     for (OBAtomIterator iter = molecule.BeginAtoms(); iter != molecule.EndAtoms(); iter++) {
-        double *coordsOfAtomI = (*iter)->GetCoordinate();
-        matrix.push_back(coordsOfAtomI[0]); matrix.push_back(coordsOfAtomI[1]); matrix.push_back(coordsOfAtomI[2]);
+        double *tmpCoords = (*iter)->GetCoordinate(); double tmpMass = (*iter)->GetAtomicMass();  
+        centerCoords[0] += tmpMass*tmpCoords[0]; centerCoords[1] += tmpMass*tmpCoords[1]; centerCoords[2] += tmpMass*tmpCoords[2];
     }
+    for (unsigned int i=0; i < centerCoords.size(); i++) centerCoords[i] /= molecule.GetMolWt();
 }
 
 void translate3DMatrixCoordinates(vector<double> &matrix, double x, double y, double z) {
@@ -54,12 +80,38 @@ void translate3DMatrixCoordinates(vector<double> &matrix, double x, double y, do
 
 void rotate3DMatrixCoordinates(vector<double> *matrix, vector<double> &rotationMatrix) {
     // both matrices must be of column-order
+    if (not matrix) { cerr << "ERROR: NO MATRIX INITIALIZED IN POINTER; EXITING" << endl; abort(); }
     vector<double> *resultMatrix = new vector<double>(matrix->size());
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, 3, matrix->size()/3, 3, 1, &rotationMatrix[0], 3, &(*matrix)[0], 3, 1, &(*resultMatrix)[0], 3);
+    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, 3, matrix->size()/3, 3, 1, &rotationMatrix[0], 3, &(*matrix)[0], 3, 0, &(*resultMatrix)[0], 3);
     delete matrix; matrix = resultMatrix;
 }
 
+void generateCovarMatrixFromMolecule(vector<double> &matrix, OBMol &molecule) {
+    double uX = 0, uY = 0, uZ = 0, *moleculeCoords = molecule.GetCoordinates();
+    for (unsigned int i=0; i < 3 * molecule.NumAtoms(); i+=3) { uX += moleculeCoords[i]; uY += moleculeCoords[i+1]; uZ += moleculeCoords[i+2]; }
+    uX /= molecule.NumAtoms(); uY /= molecule.NumAtoms(); uZ /= molecule.NumAtoms();
 
+    double cXX = 0, cYY = 0, cZZ = 0, cXY = 0, cXZ = 0, cYZ = 0;
+    for (unsigned int i=0; i < 3 * molecule.NumAtoms(); i+=3) {
+        double atomWeight = molecule.GetAtom(i/3)->GetAtomicMass();
+        cXX += pow(moleculeCoords[i] - uX, 2) * atomWeight;
+        cYY += pow(moleculeCoords[i+1] - uY, 2) * atomWeight;
+        cZZ += pow(moleculeCoords[i+2] - uZ, 2) * atomWeight;
+        cXY += (moleculeCoords[i] - uX) * (moleculeCoords[i+1] - uY) * atomWeight;
+        cXZ += (moleculeCoords[i] - uX) * (moleculeCoords[i+2] - uZ) * atomWeight;
+        cYZ += (moleculeCoords[i+1] - uY) * (moleculeCoords[i+2] - uZ) * atomWeight;
+    }
+    
+    matrix.resize(9);
+    matrix[0] = cXX; 
+    matrix[1] = matrix[3] = cXY; 
+    matrix[2] = matrix[6] = cXZ; 
+    matrix[4] = cYY;
+    matrix[5] = matrix[7] = cYZ;  
+    matrix[8] = cZZ;
+
+    for (unsigned int i=0; i < matrix.size(); i++) matrix[i] /= molecule.GetMolWt();
+}
 
 
 
@@ -70,6 +122,18 @@ int main (int argc, char **argv) {
         return 1;
     }
 
+
+    double xyz[] = {1, 2, 3, 2, 4, 5, 3, 5, 6}; 
+    vector<double> sample(xyz, &xyz[9]);
+    for (int i=0; i<sample.size(); i++) cout << sample[i] << " ";
+        cout << endl << endl << endl << endl;
+
+    vector<double> eigenvectors, eigenvalues;
+    generateEigenMatrix(eigenvectors, eigenvalues, sample);
+    for (int i=0; i<eigenvalues.size(); i++) cout << eigenvalues[i] << " ";
+        cout << endl << endl << endl << endl;
+    for (int i=0; i<eigenvectors.size(); i++) cout << eigenvectors[i] << " ";
+        cout << endl << endl << endl << endl;
 
 
     OBConversion obconversion;
