bc82db67fbbfcf7127475dfaac0852011d65eb9b
マ・ベンソン <bm@Lord-Alec-Magos-Pasions-iMac.local>
2013-06-04 18:48:22 +0900

    running batch

--- scripts/conformer.rb
+++ scripts/conformer.rb
@@ -15,6 +15,31 @@ end
 =end
 
 
+THREADS = 10
+jjobs = []
+pids = []
 
+Dir.glob("../CONFORMERS/*").delete_if { |x| x =~ /BQ123|234551|TAK044/ }.each do |fl|
+	ligand = File.basename( fl, ".*" ) # get base filename without the file extension
+	reference = "../CONFORMERS/BQ123.mol2"
+	final_struct = "../ALL_PAIRS_BQ123_AS_REFERENCE/BQ123_" + ligand + ".mol2"
+	logfile = "../ALL_PAIRS_BQ123_AS_REFERENCE/BQ123_" + ligand + ".log"
 
+	jjobs.push ["../example", reference, fl, final_struct, "&>", logfile].join " "
+end
 
+THREADS.times do
+	pids.push fork { exec jjobs.pop }
+	system "renice -20 " + pids[-1].to_s
+end
+
+while jjobs.size > 0 do 
+	pids.delete_if { |x| Process.waitpid(x, Process::WNOHANG) }
+
+	remaining = (THREADS - pids.size) > jjobs.size ? jjobs.size : (THREADS - pids.size) # in case number of open threads is larger than number of un-run trials
+	remaining.times do
+		pids.push fork { exec jjobs.pop }
+		system "renice -20 " + pids[-1].to_s
+	end
+	sleep 60
+end
