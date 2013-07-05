08ee09128fe92d69258b54e0d22777ac2eafd593
マ・ベンソン <bm@Lord-Alec-Magos-Pasions-iMac.local>
2013-07-05 18:06:57 +0900

    another run

--- scripts/randomdrawmodels.rb
+++ scripts/randomdrawmodels.rb
@@ -1,6 +1,6 @@
 require "Utils"
 
 system "rm -rf random.pdb"
-File.open("../BosentanConformers/MABENSON/BEGIN_MD/cluster_v2/1_supimp/out.pdb").collect.split_by { |x| x =~ /MODEL/ }.shuffle.first(200).each do |chunk|	
-	File.open("random.pdb", "a+") { |f| chunk.each { |x| f.write x } }
+File.open("/Users/bm/MD/bensan/cluster_v2/1_supimp/out.pdb").collect.split_by { |x| x =~ /MODEL/ }.shuffle.first(50).each do |chunk|	
+	File.open("ET1_50CONFORMERS.pdb", "a+") { |f| chunk.each { |x| f.write x } }
 end
